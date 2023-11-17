753ee4ea54fd229bf9bd76574dbffe4e6bfe4930
Christopher Brown <christopher.brown@healthstream.com>
2023-11-17 14:55:39 -0600

    made an update to Transaction interface in AppReducer to remove error

--- typescript-app/src/context/AppReducer.tsx
+++ typescript-app/src/context/AppReducer.tsx
@@ -1,25 +1,9 @@
-// export default (state, action) => {
-//   switch (action.type) {
-//     case "DELETE_TRANSACTION":
-//       return {
-//         ...state,
-//         transactions: state.transactions.filter(
-//           (transaction) => transaction.id !== action.payload
-//         ),
-//       };
-//     case "ADD_TRANSACTION":
-//       return {
-//         ...state,
-//         transactions: [action.payload, ...state.transactions],
-//       };
-//     default:
-//       return state;
-//   }
-// };
 import { Reducer } from "react";
 
 interface Transaction {
   id: number;
+  text: string;
+  amount: number;
   // Add other properties as needed
 }
 
